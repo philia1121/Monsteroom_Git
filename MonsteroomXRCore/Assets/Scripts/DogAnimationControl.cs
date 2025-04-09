d4d238f595c37f47b5c2ab8cc9697816dda5d924
lai12232 <101129234+philia1121@users.noreply.github.com>
2025-04-09 22:12:32 +0800

    Add New Project Using Meta XR Core SDK

---
+++ MonsteroomXRCore/Assets/Scripts/DogAnimationControl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogAnimationControl : MonoBehaviour
+{
+    [SerializeField]private Animator animator;
+    int MovingSpeed_Hash;
+    int IsMoving_Hash;
+    void Awake()
+    {   
+        MovingSpeed_Hash = Animator.StringToHash("MovingSpeed");
+        IsMoving_Hash = Animator.StringToHash("IsMoving");
+    }
+    void Update()
+    {
+        SetMovingingSpeed();
+        ForShots();
+    }
+
+    void SetMovingingSpeed()
+    {
+        var ins = SpeedManager.Instance;
+        var speed = (ins.Speed - ins.SpeedMin) / (ins.SpeedMax - ins.SpeedMin);
+        animator.SetFloat(MovingSpeed_Hash, speed);
+    }
+    void ForShots()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger("Yawn");
+        }
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetBool("IsSleeping", !animator.GetBool("IsSleeping"));
+        }
+    }
+}
