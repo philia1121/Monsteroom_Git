b84fb91a8d51e384af4361e3467e9e20e93abe84
philia1121 <101129234+philia1121@users.noreply.github.com>
2025-07-25 21:47:49 +0800

    Modify Dragon Scene
    Flowing Cloud Visual

--- MonsteroomOpenCVAruco/Assets/Scripts/AnyKeyStarter.cs
+++ MonsteroomOpenCVAruco/Assets/Scripts/AnyKeyStarter.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 // using Valve.VR;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class AnyKeyStarter : MonoBehaviour
 {
     // public SteamVR_Action_Boolean interactAction;
     // [SerializeField]private bool keyboard_debug;
     public UnityEvent OnAnyKeyEvent;
-    // [SerializeField]private bool showLog = false;
+    MyInputMap myInputMap;
+    [SerializeField] private bool showLog = false;
 
     // void Update()
     // {
@@ -19,4 +21,28 @@ public class AnyKeyStarter : MonoBehaviour
     //         if (showLog) Debug.Log("Action 'interact' triggered!");
     //     }
     // }
+    void Awake()
+    {
+        myInputMap = new MyInputMap();
+    }
+    void OnEnable()
+    {
+        myInputMap.Gameplay.Enable();
+        myInputMap.Gameplay.AnyInput.started += ctx => OnAnyKey();
+    }
+
+    void Start()
+    {
+
+    }
+    void OnDisable()
+    {
+        myInputMap.Gameplay.Disable();
+    }
+
+    void OnAnyKey()
+    {
+        OnAnyKeyEvent?.Invoke();
+        if (showLog) Debug.Log("Action 'interact' triggered!");
+    }
 }
