709f05384ec6fefc015f620fcc702c7b46ef7e82
philia1121 <101129234+philia1121@users.noreply.github.com>
2025-08-02 21:42:13 +0800

    Tracking Distance Threshold

--- MonsteroomOpenCVAruco/Assets/Scripts/TrackingEventCaller.cs
+++ MonsteroomOpenCVAruco/Assets/Scripts/TrackingEventCaller.cs
@@ -1,71 +1,167 @@
 using System.Collections;
 using System.Collections.Generic;
+using Meta.XR.ImmersiveDebugger.UserInterface.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 public class TrackingEventCaller : MonoBehaviour
 {
+    MyInputMap myInputMap;
     [SerializeField] private Transform trackingTarget;
+    [SerializeField] private Transform playerTransform;
     [SerializeField] private IgnoreVector3Option moveIgnore;
     [SerializeField] private IgnoreVector3Option RotationIgnore;
+    [SerializeField] private float timeWindow;
     [SerializeField] private float moveThreshold;
     [SerializeField] private float moveThresholdTime;
     float moveTimer;
     [SerializeField] private float rotateThreshold;
     [SerializeField] private float rotateThresholdTime;
     float rotateTimer;
+    [SerializeField] private float playerMoveThreshold;
+    [SerializeField] private float playerMoveThresholdTime;
+    float playerMoveTimer;
+
+    [Header("UI Board")]
+    public TextMesh tm;
+    public UnityEngine.UI.Image move_img, rotate_img, playerMove_img, playerRotate_img;
+
     Vector3 currentPos, oldPos;
     Quaternion currentRot, oldRot;
+    Vector3 playerCurrentPos, playerOldPos;
+    Quaternion playerCurrentRot, playerOldRot;
     public bool IsMoving;
-    bool wasMoing;
+    bool wasMoving;
     public bool IsRotating;
     bool wasRotating;
+    public bool IsPlayerMoving;
+    bool wasPlayerMoving;
+    public bool IsPlayerRotating;
+    bool wasPlayerRotating;
+    float minMovingDist, maxMovingDist, minRotatingDist, maxRotatingDist, movingDist, rotatingDist;
+    LinkedList<(float time, Vector3 pos)> historyPos = new LinkedList<(float time, Vector3 pos)>();
+    void Awake()
+    {
+        myInputMap = new MyInputMap();
+    }
+    void OnEnable()
+    {
+        myInputMap.TestKey.Enable();
+        myInputMap.TestKey.NextClip.started += ctx => ResetValue();
+    }
+    void ResetValue()
+    {
+        minMovingDist = 0;
+        maxMovingDist = 0;
+        minRotatingDist = 0;
+        maxRotatingDist = 0;
+        ShowText();
+    }
 
     void Update()
     {
         currentPos = FilteredPos(trackingTarget.position);
         currentRot = FilteredRotaion(trackingTarget.rotation);
+        playerCurrentPos = FilteredPos(playerTransform.position);
+        playerCurrentRot = FilteredRotaion(playerTransform.rotation);
 
         CheckMovingState();
         CheckRotatingState();
+        CheckPlayerMovingState();
+        CheckPlayerRotatingState();
+
+        ShowText();
+        ShowStateImage();
 
         oldPos = currentPos;
         oldRot = currentRot;
+        playerOldPos = playerCurrentPos;
+        playerOldRot = playerCurrentRot;
     }
 
 
     void CheckMovingState()
     {
-        // Debug.Log(Vector3.Distance(currentPos, oldPos));
-        if (Vector3.Distance(currentPos, oldPos) > moveThreshold)
+        // if (Vector3.Distance(currentPos, oldPos) > moveThreshold)
+        // {
+        //     moveTimer += Time.deltaTime;
+        //     if (moveThresholdTime >= moveTimer)
+        //     {
+        //         IsMoving = true;
+        //     }
+        // }
+        // else
+        // {
+        //     moveTimer = 0;
+        //     IsMoving = false;
+        // }
+
+        // if (IsMoving != wasMoving)
+        // {
+        //     Debug.Log("Moving State Change: " + IsMoving);
+        // }
+        // wasMoving = IsMoving;
+        float now = Time.time;
+        historyPos.AddLast((now, currentPos));
+        while (historyPos.Count > 0 && now - historyPos.First.Value.time > timeWindow + 0.05f)
         {
-            moveTimer += Time.deltaTime;
-            if (moveThresholdTime >= moveTimer)
-            {
-                IsMoving = true;
-            }
-        }
-        else
-        {
-            moveTimer = 0;
-            IsMoving = false;
+            historyPos.RemoveFirst();
         }
 
-        if (IsMoving != wasMoing)
+        foreach (var record in historyPos)
         {
-            Debug.Log("Moving State Change: " + IsMoving);
+            float age = now - record.time;
+            if (age >= timeWindow)
+            {
+                movingDist = Vector3.Distance(currentPos, record.pos);
+            }
         }
-        wasMoing = IsMoving;
     }
     void CheckRotatingState()
     {
-        if (Vector3.Distance(currentRot.eulerAngles, oldRot.eulerAngles) > rotateThreshold)
-        {
 
-        }
-        else
-        {
+    }
+    void CheckPlayerMovingState()
+    {
 
+    }
+    void CheckPlayerRotatingState()
+    {
+
+    }
+
+    float DistanceBetween()
+    {
+        return Vector3.Distance(currentPos, playerCurrentPos);
+    }
+    void ShowText()
+    {
+        // var movingDist = Vector3.Distance(currentPos, oldPos);
+        if (movingDist > 0)
+        {
+            if (minMovingDist == 0) minMovingDist = movingDist;
+            if (movingDist < minMovingDist) minMovingDist = movingDist;
+            if (movingDist > maxMovingDist) maxMovingDist = movingDist;
         }
+
+        string t = "Pos Difference: " + movingDist + "\n"
+            + "Rot Difference: " + Vector3.Distance(currentRot.eulerAngles, oldRot.eulerAngles) + "\n"
+            + "Player Pos Difference: " + Vector3.Distance(currentPos, oldPos) + "\n"
+            + "Player Rot Difference: " + Vector3.Distance(playerCurrentRot.eulerAngles, playerOldRot.eulerAngles) + "\n"
+            + "\n"
+            + "Min Moving Distance: " + minMovingDist + "\n"
+            + "Max Moving Distance: " + maxMovingDist + "\n"
+            + "Min Rotating Distance: " + minRotatingDist + "\n"
+            + "Max Rotating Distance: " + minRotatingDist + "\n";
+        tm.text = t;
+    }
+    void ShowStateImage()
+    {
+        move_img.color = Vector3.Distance(currentPos, oldPos) > 0 ? Color.green : Color.red;
+        rotate_img.color = Vector3.Distance(currentRot.eulerAngles, oldRot.eulerAngles) > 0 ? Color.green : Color.red;
+        playerMove_img.color = Vector3.Distance(currentPos, oldPos) > 0 ? Color.green : Color.red;
+        playerRotate_img.color = Vector3.Distance(playerCurrentRot.eulerAngles, playerOldRot.eulerAngles) > 0 ? Color.green : Color.red;
     }
     Vector3 FilteredPos(Vector3 value)
     {
@@ -82,7 +178,6 @@ public class TrackingEventCaller : MonoBehaviour
         float newZ = moveIgnore.ignoreZ ? 0 : rot.z;
         return Quaternion.Euler(new Vector3(newX, newY, newZ));
     }
-
 }
 
 [System.Serializable]
