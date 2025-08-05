56319a1e9c0112b8c4d9f92b03b409ce23c2c02d
philia1121 <101129234+philia1121@users.noreply.github.com>
2025-08-05 21:26:15 +0800

    Remove Unused Project

--- MonsteroomOpenCVAruco_Dog/Assets/JC_Express/Human_Friendly/SimpleNote.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SimpleNote : MonoBehaviour
-{
-    [TextArea(5, 10)]
-    public string Notes = "Comment Here.";
-}
